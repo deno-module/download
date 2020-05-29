8e18054929123cfb0c4f96f0cac1bd2eab7392b4
Karthik Thayyil <me@kt12.in>
2020-05-29 09:18:26 +0530

    Remove type in direct assignment| Simplify fn Doc.

--- download.ts
+++ download.ts
@@ -1,14 +1,8 @@
 import { Destination, DownlodedFile } from "./types.ts";
+// TODO(kt-12): Enable ensure dir once stable.
 // import { ensureDirSync } from "https://deno.land/std/fs/mod.ts"
 
-/**
- * Download file from url to the destination.
- *
- * @param {any} url:string|URL url of the file to be donwloaded.
- * @param {any} destination?:Destination Destination details do the downloaded file.
- * @param {any} options?:RequestInit same as fetch api RequestInit, for passing headers and other options.
- * @returns {any} DownlodedFile: gives path and other information of the downloaded file or Error if failure.
- */
+/** Download file from url to the destination. */
 export async function download(
   url:string|URL,
   destination?:Destination,
@@ -21,18 +15,18 @@ export async function download(
     let finalUrl:string;
     let size:number;
 
-    const response: Response = await fetch(url, options);
+    const response = await fetch(url, options);
     finalUrl = response.url.replace(/\/$/, "");
     if(response.status != 200){
       return Promise.reject(
         new Deno.errors.Http(`status ${response.status}-'${response.statusText}' received instead of 200`)
       );
     }
-    const blob: Blob = await response.blob();
+    const blob = await response.blob();
     /** size in bytes */
     size = blob.size;
-    const buffer: ArrayBuffer = await blob.arrayBuffer();
-    const unit8arr: Uint8Array = new Deno.Buffer(buffer).bytes();
+    const buffer = await blob.arrayBuffer();
+    const unit8arr = new Deno.Buffer(buffer).bytes();
     if( typeof destination === 'undefined' || typeof destination.dir === 'undefined' ){
       dir = Deno.makeTempDirSync({ prefix: 'deno_dwld' });
     } else {
