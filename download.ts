216ab18b34c5755227e187f8777e322e19f0d86f
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-04-16 13:46:01 +0200

    format download.ts with deno fmt

--- download.ts
+++ download.ts
@@ -4,48 +4,57 @@ import { Destination, DownlodedFile } from "./types.ts";
 
 /** Download file from url to the destination. */
 export async function download(
-  url:string|URL,
-  destination?:Destination,
-  options?:RequestInit
-): Promise<DownlodedFile>{
-    let file:string;
-    let fullPath:string;
-    let dir:string = '';
-    let mode:object = {};
-    let finalUrl:string;
-    let size:number;
+  url: string | URL,
+  destination?: Destination,
+  options?: RequestInit,
+): Promise<DownlodedFile> {
+  let file: string;
+  let fullPath: string;
+  let dir: string = "";
+  let mode: object = {};
+  let finalUrl: string;
+  let size: number;
 
-    const response = await fetch(url, options);
-    finalUrl = response.url.replace(/\/$/, "");
-    if(response.status != 200){
-      return Promise.reject(
-        new Deno.errors.Http(`status ${response.status}-'${response.statusText}' received instead of 200`)
-      );
-    }
-    const blob = await response.blob();
-    /** size in bytes */
-    size = blob.size;
-    const buffer = await blob.arrayBuffer();
-    const unit8arr = new Deno.Buffer(buffer).bytes();
-    if( typeof destination === 'undefined' || typeof destination.dir === 'undefined' ){
-      dir = Deno.makeTempDirSync({ prefix: 'deno_dwld' });
-    } else {
-      dir = destination.dir;
-    }
-    if(typeof destination === 'undefined' || typeof destination.file === 'undefined' ){
-      file = finalUrl.substring(finalUrl.lastIndexOf('/')+1);
-    } else {
-      file = destination.file;
-    }
-    if(typeof destination != 'undefined' && typeof destination.mode != 'undefined' ){
-        mode = { mode: destination.mode }
-    }
+  const response = await fetch(url, options);
+  finalUrl = response.url.replace(/\/$/, "");
+  if (response.status != 200) {
+    return Promise.reject(
+      new Deno.errors.Http(
+        `status ${response.status}-'${response.statusText}' received instead of 200`,
+      ),
+    );
+  }
+  const blob = await response.blob();
+  /** size in bytes */
+  size = blob.size;
+  const buffer = await blob.arrayBuffer();
+  const unit8arr = new Deno.Buffer(buffer).bytes();
+  if (
+    typeof destination === "undefined" || typeof destination.dir === "undefined"
+  ) {
+    dir = Deno.makeTempDirSync({ prefix: "deno_dwld" });
+  } else {
+    dir = destination.dir;
+  }
+  if (
+    typeof destination === "undefined" ||
+    typeof destination.file === "undefined"
+  ) {
+    file = finalUrl.substring(finalUrl.lastIndexOf("/") + 1);
+  } else {
+    file = destination.file;
+  }
+  if (
+    typeof destination != "undefined" && typeof destination.mode != "undefined"
+  ) {
+    mode = { mode: destination.mode };
+  }
 
-    dir = dir.replace(/\/$/, "");
-    // TODO(kt-12): Enable ensureDirSync once stable.
-    // ensureDirSync(dir)
+  dir = dir.replace(/\/$/, "");
+  // TODO(kt-12): Enable ensureDirSync once stable.
+  // ensureDirSync(dir)
 
-    fullPath = `${dir}/${file}`;
-    Deno.writeFileSync(fullPath, unit8arr, mode);
-    return Promise.resolve({file, dir, fullPath, size});
+  fullPath = `${dir}/${file}`;
+  Deno.writeFileSync(fullPath, unit8arr, mode);
+  return Promise.resolve({ file, dir, fullPath, size });
 }
